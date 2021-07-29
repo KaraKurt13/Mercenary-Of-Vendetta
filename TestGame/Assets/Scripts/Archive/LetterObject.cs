d5fd49a6fd6d9fc5d02bfd61e30388e5ee53897f
KaraKurt13 <65405277+KaraKurt13@users.noreply.github.com>
2021-07-29 18:25:24 +0300

    Объект письма
    - Создан скрипт объекта письма;
    -  Добавлено письмо на сцену Дом_1

---
+++ TestGame/Assets/Scripts/Archive/LetterObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterObject : MonoBehaviour
+{
+    [SerializeField] LettersInfo LettersList;
+    [SerializeField] int letter_id;
+    [SerializeField] GameObject PlayerObj;
+    private BoxCollider2D PlayerBox;
+    private BoxCollider2D TriggerBox;
+
+
+    void Start()
+    {
+        PlayerBox = PlayerObj.GetComponent<BoxCollider2D>();
+        TriggerBox = this.GetComponent<BoxCollider2D>();
+        
+    }
+
+    void Update()
+    {
+        if (PlayerBox.IsTouching(TriggerBox))
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                AddLetter(letter_id);
+                Destroy(gameObject);
+            }
+        }
+    }
+    void AddLetter(int letter_id)
+    {
+        LettersList.LettersIsDiscovered[letter_id] = true;
+    }
+
+}
